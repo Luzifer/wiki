e1d545aca7461970c7e15800da331b01ce8f615f
Knut Ahlers <knut@ahlers.me>
2019-08-15 22:38:19 +0200

    Add top navigation
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- src/app.vue
+++ src/app.vue
@@ -11,6 +11,25 @@
       >
         Wiki
       </b-navbar-brand>
+
+      <b-navbar-nav>
+        <b-nav-item
+          v-for="page in navContent"
+          :key="page"
+          :to="{ name: 'view', params: { page } }"
+        >
+          {{ page }}
+        </b-nav-item>
+      </b-navbar-nav>
+
+      <template ref="nav">
+        <vue-markdown
+          class="nav"
+          :source="navContent"
+          :prerender="prerender"
+          @rendered="rendered"
+        />
+      </template>
     </b-navbar>
 
     <router-view />
@@ -18,7 +37,40 @@
 </template>
 
 <script>
+import axios from 'axios'
+import VueMarkdown from 'vue-markdown'
+
 export default {
   name: 'App',
+
+  components: {
+    VueMarkdown,
+  },
+
+  data() {
+    return {
+      navContent: '',
+    }
+  },
+
+  mounted() {
+    this.loadNav()
+  },
+
+  methods: {
+    loadNav() {
+      axios.get(`/_content/_navigation`)
+        .then(resp => {
+          this.navContent = resp.data.content.split('\n')
+            .filter(el => !el.match(/^!/))
+        })
+        .catch(err => {
+          if (err.response && err.response.status === 404) {
+            return
+          }
+          console.error(err)
+        })
+    },
+  },
 }
 </script>
